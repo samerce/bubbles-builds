ceb18a8061e54330f25de8f2107b4785fd6dd275
bubbles <samerce@gmail.com>
2022-01-14 21:12:11 -0600

    new bio!

--- components/SiteMenu.js
+++ components/SiteMenu.js
@@ -1,32 +1,38 @@
 import React from "react"
 import { PopupRoot, Subheader, Section, SectionTitle, Button } from "./Basics"
-import usePopup from "../model/usePopup"
+import usePopup, { Popups } from "../model/usePopup"
 
-export default function SiteMenu(p) { return pug`
-  PopupRoot.bg-tpBlack.rounded-2xl(...p className=p.className)
-    Subheader.border-b.border-tpWhite.bg-accent.rounded-t-2xl
-      span(class='text-xl xs:text-2xl sm:text-3xl') Bubbles Builds!
+export default function SiteMenu(p) { 
+  const {showPopup} = usePopup()
 
-    div.w-full.grow.flex.flex-col.overflow-y-scroll.overflow-x-hidden.bg-accent.rounded-b-2xl
-      
-      MenuButton(id='bio', rotate=3) Who is Bubbles?
+  return pug`
+    PopupRoot.bg-tpBlack.rounded-2xl(...p className=p.className)
+      Subheader.border-b.border-tpWhite.bg-accent.rounded-t-2xl
+        span(class='text-xl xs:text-2xl sm:text-3xl') Bubbles Builds!
 
-      MenuButton(id='expressyouryes' rotate=-1) Express Your Yes
+      div.w-full.grow.flex.flex-col.overflow-y-scroll.overflow-x-hidden.bg-accent.rounded-b-2xl
+        
+        MenuButton(id='bio' rotate=3 onClick=${() => {
+          setTimeout(() => showPopup(Popups.WhoIsBubbles), 400)
+        }}) Who is Bubbles?
 
-      MenuButton(id='rickyforhouse' rotate=2) Ricky for House
-      
-      MenuButton(id='acupuncture' rotate=-3) Acupuncture
+        MenuButton(id='expressyouryes' rotate=-1) Express Your Yes
 
-      MenuButton(id='dqitwh' rotate=1) DQITWH
+        MenuButton(id='rickyforhouse' rotate=2) Ricky for House
+        
+        MenuButton(id='acupuncture' rotate=-3) Acupuncture
 
-      MenuButton(id='flitandland' rotate=-2) Flit & Land
+        MenuButton(id='dqitwh' rotate=1) DQITWH
 
-      MenuButton(id='psymail' rotate=3) Psymail
+        MenuButton(id='flitandland' rotate=-2) Flit & Land
 
-      MenuButton(id='glyphite' rotate=-1) Glyphite
-      
-      MenuButton(id='bjg' rotate=2) Blackjack Genius
-`}
+        MenuButton(id='psymail' rotate=3) Psymail
+
+        MenuButton(id='glyphite' rotate=-1) Glyphite
+        
+        MenuButton(id='bjg' rotate=2) Blackjack Genius
+  `
+}
 
 var MenuButton = p => {
   const {hidePopup} = usePopup()
@@ -35,13 +41,14 @@ var MenuButton = p => {
     hidePopup()
     setTimeout(() => {
       window.location = '#' + id
+      p.onClick && p.onClick()
     }, 200)
   }
 
   return pug`
     button(
       class='basis-[81px] shrink-0 border-b border-tpWhite cursor-pointer w-full flex-center'
-      onClick=() => onClick(p.id) 
+      onClick=() => onClick(p.id)
     )
       div.font-button.text-3xl.text-shadow-duo.text-accent.select-none.bg-white.px-2.py-1.rounded-xl(style=${{
         transform: `rotate(${p.rotate}deg)`,
--- components/WhoIsBubbles.js
+++ components/WhoIsBubbles.js
@@ -1,13 +1,18 @@
 import React from "react"
-import { PopupRoot, Subheader, Section, SectionTitle, Button } from "./Basics"
+import { PopupRoot, Subheader, Section, SectionTitle, Button, Link } from "./Basics"
 import Image from 'next/image'
 import usePopup from '../model/usePopup'
 
 export default function WhoIsBubbles(p) { 
   const {hidePopup} = usePopup()
 
+  function goToAnchor(id) {
+    hidePopup()
+    setTimeout(() => window.location = '#' + id, 200)
+  }
+
   return pug`
-    PopupRoot.overflow-visible(...p className=p.className)
+    PopupRoot(...p className=p.className + ' mt-[180px]')
 
       div(class='absolute right-0 w-[320px] h-[180px] -translate-y-full pointer-events-none')
         Image(src='https://s3.us-east-2.amazonaws.com/purplerepublic.us/commons/bubbles-hi.gif' width=320 height=180 quality=90)
@@ -16,8 +21,16 @@ export default function WhoIsBubbles(p) {
         | Who is Bubbles?
 
       div.w-full.grow.flex.flex-col.overflow-y-scroll.glass-dark.p-7
-        p Bubbles (they/them) is a former software engineer for corporate giants. After leaving the inhumane conditions of corporate life, they traveled the world and began developing new skills. Now, Bubbles produces music, writes, and is developing their skills in graphic design.
-
+        p Bubbles Sandcastle has a BSE in Computer Science from the University of Michigan. Bubbles formerly worked in Big Tech and left that world due to its poor work-life balance and general lack of social consciousness.
+        br
+        p After leaving Amazon.com, Bubbles explored the startup scene and eventually founded one of their own, Clover Labs. There, they built a web-based game-making platform and used it to launch #[a(onClick=() => goToAnchor('bjg') class='cursor-pointer') Blackjack Genius], an Android game that held a top-20 spot in the Play Store for over a year. 
+        br
+        p Bubbles ended their time in Silicon Valley working for #[a(target='_blank' href='https://iodine.com') Iodine.com], a health-tech startup focused on crowdsourcing medication information and helping people with depression find the right medication faster. This was a step in the right direction, yet something still felt off. 
+        br
+        p Having hip-hop danced their way out of depression, Bubbles decided to take a break from tech and explore the arts. They joined #[a Petals Sandcastle] at the tail-end of #[a(target='_blank' href='https://www.youtube.com/watch?v=ljrsFO7VZro') Lampshade Cafe] and years later co-founded #[a(target='_blank' href='https://www.expressyouryes.com') Express Your Yes Foundation] to amplify marginalized voices and advocate for art as healing.
+        br
+        p Now, Bubbles co-runs Express Your Yes Foundation, #[a(target='_blank' href='https://soundcloud.com/expressyouryes') produces music], soaks in the sun, #[a(target='_blank' href='https://ripplewiggle.medium.com') writes], experiments with sustainable living practices, dabbles in graphic design, and builds websites & apps for creatives, healers, nonprofits, and other arts-centered, socially responsible and environmentally conscious agents.
+        
       Subheader.border-t.border-tpWhite.bg-accent.rounded-b-2xl.relative.p-0.h-54
         button.w-full.h-full.font-button.text-white.border-none(onClick=hidePopup) Close
   `
