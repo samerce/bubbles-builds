888db5ddeb7c5e60dfcc1127309fec46e99a985b
bubbles <samerce@gmail.com>
2022-01-23 19:37:38 -0600

    deferred loading for offscreen iframes

--- components/Page.js
+++ components/Page.js
@@ -1,8 +1,10 @@
 import { VSnapItem } from './Stack'
 import useNav from '../model/useNav'
 import usePopup from '../model/usePopup'
+import { useEffect, useState } from 'react'
 
 export default function Page(p) {
+  const [hasLoaded, setHasLoaded] = useState(false)
   const {page, pageDidAppear} = useNav()
   const {popupId} = usePopup()
   
@@ -16,21 +18,23 @@ export default function Page(p) {
     })
   }
 
-  function visibility() {
-    // if (p.index > page.index + 1 || p.index < page.index - 1 ||
-    //   (p.index !== page.index && popupId)) {
-    //   return 'hidden'
-    // } else 
-      return 'visible'
+  function canLoad() {
+    return hasLoaded || p.index === page.index + 1 || p.index === 0
   }
 
+  useEffect(() => {
+    if (canLoad() && !hasLoaded) {
+      setHasLoaded(true)
+    }
+  }, [page.index])
+
   return (
-    <VSnapItem {...p} id={p.id} onAppear={onAppear} style={{visibility: visibility()}} 
+    <VSnapItem {...p} id={p.id} onAppear={onAppear} 
     className={p.className + ' w-full h-full relative flex flex-col items-center pb-nav md:pb-navBig'}>
 
       <iframe 
         className='pointer-events-none absolute-full w-full h-full'
-        src={`/shader/shader.html?id=${p.shaderId}`}
+        src={canLoad()? `/shader/shader.html?id=${p.shaderId}` : ''}
         ariaHidden
       />
       {p.children}
