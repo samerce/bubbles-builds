c4543ee19e5707b1563f81de543e7294f60a6dd7
bubbles <samerce@gmail.com>
2022-01-23 20:12:56 -0600

    removed waving bubbles from thanks page

--- components/Thanks.js
+++ components/Thanks.js
@@ -19,10 +19,7 @@ export default function Thanks(p) {
   usePopupScrollReset(ScrollerId, Popups.Thanks)
 
   return pug`
-    PopupRoot(...p className=p.className + ' mt-[90px]')
-
-      div(class='absolute right-0 w-[160px] h-[90px] -translate-y-full pointer-events-none')
-        Image(src='https://s3.us-east-2.amazonaws.com/purplerepublic.us/commons/bubbles-hi.gif' width=160 height=90 quality=90)
+    PopupRoot(...p)
 
       Subheader.border-b.border-b-tpWhite.bg-accent.rounded-t-2xl
         | A Trillion Thanks!
@@ -32,13 +29,11 @@ export default function Thanks(p) {
         id=ScrollerId
       )
         ThanksSection 
-          div.flex.pb-2
-            SectionTitle.rotate-2 friends & family
+          SectionTitle.rotate-2.mb-2 friends & family
           Text My work and I are not possible without my loving family, including my partner Petals, my mom Priscilla, my brother Rameen, my sister Alyana, and some awesome friends who helped me brainstorm and test: Koki, T, and Abby.
 
         ThanksSection.mt-0
-          div.flex.pb-2
-            SectionTitle.rotate-3 shaders
+          SectionTitle.rotate-3.mb-2 shaders
           Text The gorgeous backgrounds on this site are shaders from creators on Shadertoy.com. Huge thanks to their painstaking work developing the equations that make these stunning pieces of art. It still blows my mind that they’re made with only math and sometimes some images of just “noise”. So grateful the following talented humans have offered their work for free:
           ItemGroup
             Item(href='https://www.shadertoy.com/user/FabriceNeyret2') Fabrice Neyret 2 (Landing)
@@ -53,8 +48,7 @@ export default function Thanks(p) {
             Item(href='https://www.shadertoy.com/user/Nicolas2') Nicolas 2 (Let's Build)
 
         ThanksSection
-          div.flex.pb-2
-            SectionTitle.-rotate-1 fonts
+          SectionTitle.-rotate-1.mb-2 fonts
           Text Anyone who knows me, knows I obsess over fonts. They can make or break a design and I spent several hours picking and re-picking fonts for this site. Many thanks to the following designers (and Google Fonts) who offer their impeccable fonts for free:
           ItemGroup
             Item(href='https://github.com/Fonthausen/CrimsonPro') Jacques Le Bailly (Crimson Pro)
@@ -62,8 +56,7 @@ export default function Thanks(p) {
             Item(href='https://fonts.google.com/specimen/Bayon?query=Bayon#about') Danh Hong (Bayon)
 
         ThanksSection
-          div.flex.pb-2
-            SectionTitle.-rotate-1 icons
+          SectionTitle.-rotate-1.mb-2 icons
           Text The icons are generously provided by:
           ItemGroup
             Item(href='https://github.com/react-icons/react-icons') Font Awesome via React Icons
